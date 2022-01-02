73f181326d76bf6388f4d47070c10a41b0a10f3a
Aadil Matloob Jan <38073631+aadil494@users.noreply.github.com>
2022-01-02 22:12:19 +0530

    Create quotes.py

---
+++ QuotesScraperWithPython/quotes.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import argparse
+from random import choice
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-q",help="for search term")
+parser.add_argument("-r",help="if you want one random quote")
+
+args = parser.parse_args()
+
+
+
+search_term = args.q
+page=1
+url = f"https://www.brainyquote.com/search_results?q={search_term}&pg={page}"
+
+
+r = requests.get(url)
+soup = bs(r.content,"html.parser")
+anchor1 = soup.find_all('a',{"class":"b-qt"})
+anchor2 = soup.find_all('a',{"class":"bq-aut"})
+
+
+if args.r != None :
+    quote = choice([f"{quote.text.strip()} - {author.text.strip()}" for quote,author in zip(anchor1,anchor2)])
+    print(quote)
+else:
+    for i,(quote,author) in enumerate(zip(anchor1,anchor2)):
+        quote = quote.find('div').text.strip()
+        author = author.text.strip()
+        print(f"[{i}] : {quote} - {author}")
+
+
